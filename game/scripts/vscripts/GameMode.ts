9fb1a620a97c310a49b4500593ff1e12856d3681
Poddo <mepoddo@gmail.com>
2021-04-19 15:08:50 -0500

    cleanup some starting garbage

--- game/scripts/vscripts/GameMode.ts
+++ game/scripts/vscripts/GameMode.ts
@@ -1,5 +1,4 @@
 import { reloadable } from "./lib/tstl-utils";
-import "./modifiers/modifier_panic";
 
 const heroSelectionTime = 10;
 
@@ -12,8 +11,6 @@ declare global {
 @reloadable
 export class GameMode {
     public static Precache(this: void, context: CScriptPrecacheContext) {
-        PrecacheResource("particle", "particles/units/heroes/hero_meepo/meepo_earthbind_projectile_fx.vpcf", context);
-        PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_meepo.vsndevts", context);
     }
 
     public static Activate(this: void) {
@@ -23,7 +20,6 @@ export class GameMode {
     constructor() {
         this.configure();
         ListenToGameEvent("game_rules_state_change", () => this.OnStateChange(), undefined);
-        ListenToGameEvent("npc_spawned", event => this.OnNpcSpawned(event), undefined);
     }
 
     private configure(): void {
@@ -36,13 +32,6 @@ export class GameMode {
 
     public OnStateChange(): void {
         const state = GameRules.State_Get();
-
-        // Add 4 bots to lobby in tools
-        if (IsInToolsMode() && state == DOTA_GameState.DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP) {
-            for (let i = 0; i < 4; i++) {
-                Tutorial.AddBot("npc_dota_hero_lina", "", "", false);
-            }
-        }
 
         // Start game once pregame hits
         if (state == DOTA_GameState.DOTA_GAMERULES_STATE_PRE_GAME) {
@@ -61,20 +50,5 @@ export class GameMode {
         print("Script reloaded!");
 
         // Do some stuff here
-    }
-
-    private OnNpcSpawned(event: NpcSpawnedEvent) {
-        // After a hero unit spawns, apply modifier_panic for 8 seconds
-        const unit = EntIndexToHScript(event.entindex) as CDOTA_BaseNPC; // Cast to npc since this is the 'npc_spawned' event
-        if (unit.IsRealHero()) {
-            Timers.CreateTimer(1, () => {
-                unit.AddNewModifier(unit, undefined, "modifier_panic", { duration: 8 });
-            });
-
-            if (!unit.HasAbility("meepo_earthbind_ts_example")) {
-                // Add lua ability to the unit
-                unit.AddAbility("meepo_earthbind_ts_example");
-            }
-        }
     }
 }
