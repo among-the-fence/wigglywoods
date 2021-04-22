26e8c054270792beda3586810b7f7baeda18af4f
Poddo <mepoddo@gmail.com>
2021-04-22 11:02:48 -0500

    working version w/ turbo and creep buff

--- game/scripts/vscripts/GameMode.ts
+++ game/scripts/vscripts/GameMode.ts
@@ -1,6 +1,11 @@
 import { reloadable } from "./lib/tstl-utils";
 
 const heroSelectionTime = 10;
+let GOLD_MODIFIER = 2;
+let XP_MODIFIER = 2;
+let RESPAWN_MODIFIER = .5;
+let command_listener: EventListenerID
+let CREEP_SCALE = .15;
 
 declare global {
     interface CDOTAGamerules {
@@ -10,6 +15,8 @@ declare global {
 
 @reloadable
 export class GameMode {
+    Game: CDOTABaseGameMode = GameRules.GetGameModeEntity();
+
     public static Precache(this: void, context: CScriptPrecacheContext) {
     }
 
@@ -23,11 +30,33 @@ export class GameMode {
     }
 
     private configure(): void {
+        this.setGameRules();
+        this.setFilters();
+        this.setListeners();
+    }
+
+    setListeners()
+    {
+        ListenToGameEvent("npc_spawned", event => this.onNPCSpawned(event), undefined);
+        command_listener = ListenToGameEvent("player_chat", event => this.onPlayerChat(event), undefined);
+    }
+
+    setFilters() {
+        this.Game.SetModifyGoldFilter(event => this.modifyGoldFilter(event), this);
+        this.Game.SetModifyExperienceFilter(event => this.modifyXPFilter(event), this);
+        this.Game.SetBountyRunePickupFilter(event => this.modifyBountyFilter(event), this);
+    }
+    
+    
+    setGameRules() {
+        GameRules.SetFilterMoreGold(true);        
         GameRules.SetCustomGameTeamMaxPlayers(DOTATeam_t.DOTA_TEAM_GOODGUYS, 3);
         GameRules.SetCustomGameTeamMaxPlayers(DOTATeam_t.DOTA_TEAM_BADGUYS, 3);
-
-        GameRules.SetShowcaseTime(0);
+        GameRules.SetShowcaseTime(0);        
         GameRules.SetHeroSelectionTime(heroSelectionTime);
+        GameRules.SetGoldTickTime(1000);
+        this.Game.SetRespawnTimeScale(RESPAWN_MODIFIER);
+        this.Game.SetFreeCourierModeEnabled(true);
     }
 
     public OnStateChange(): void {
@@ -41,14 +70,105 @@ export class GameMode {
 
     private StartGame(): void {
         print("Game starting!");
-
+        Timers.CreateTimer(90, () => this.DeleteCommandListener());
         // Do some stuff here
+    }
+    DeleteCommandListener(): number | void {
+        print("Removing Command Listener!")
+        StopListeningToGameEvent(command_listener);
+        GameRules.SendCustomMessage("Game Rules Set!", 0, 0)
+        GameRules.SendCustomMessage("Gold Scale: " + GOLD_MODIFIER, 1, 1)
+        GameRules.SendCustomMessage("XP Scale: " + XP_MODIFIER, 0, 0)
+        GameRules.SendCustomMessage("Respawn Scale: " + RESPAWN_MODIFIER, 0, 0)
+        GameRules.SendCustomMessage("Creep Scale: " + CREEP_SCALE, 0, 0)
     }
 
     // Called on script_reload
     public Reload() {
-        print("Script reloaded!");
-
         // Do some stuff here
     }
+
+    modifyXPFilter(event: ModifyExperienceFilterEvent): boolean {
+        let xp = event.experience;
+        event.experience = xp * XP_MODIFIER;
+
+        return true;
+    }
+
+    modifyGoldFilter(event: ModifyGoldFilterEvent): boolean {
+        let gold = event.gold;
+        //ignore things like selling/buying items, buyback, etc
+        if (event.reason_const < 10){
+            return true;
+        }
+
+        event.gold = gold * GOLD_MODIFIER;
+
+        return true;
+    }
+
+    modifyBountyFilter(event: BountyRunePickupFilterEvent): boolean {
+        let gold = event.gold_bounty;
+        event.gold_bounty = gold * GOLD_MODIFIER;
+
+        return true;
+    }
+
+    onNPCSpawned(event: NpcSpawnedEvent)
+    {
+        let unit = EntIndexToHScript(event.entindex) as CDOTA_BaseNPC;
+
+        // if (unit.GetName() == "npc_dota_roshan")
+        // {
+        //     unit.AddAbility("roshan_multiply");
+        //     unit.FindAbilityByName("roshan_multiply")?.SetLevel(1);
+        // }
+
+        if (unit.IsCreep() || unit.IsNeutralUnitType())
+        {
+            let game_minute = Math.floor(GameRules.GetDOTATime(false, false)/60);
+            unit.SetBaseDamageMin(unit.GetBaseDamageMin()*((CREEP_SCALE * game_minute)+1));
+            unit.SetBaseDamageMax(unit.GetBaseDamageMax()*((CREEP_SCALE * game_minute)+1));
+            unit.SetMaxHealth(unit.GetMaxHealth()*((CREEP_SCALE * game_minute)+1));
+            unit.SetHealth(unit.GetHealth()*((CREEP_SCALE * game_minute)+1));
+            unit.SetPhysicalArmorBaseValue(unit.GetPhysicalArmorBaseValue()*((CREEP_SCALE * game_minute)+1));
+
+        }
+
+
+    }
+
+    onPlayerChat(event: PlayerChatEvent)
+    {
+        if (event.text[0] == "-" && GameRules.PlayerHasCustomGameHostPrivileges(PlayerResource.GetPlayer(event.playerid)!))
+        {
+            let msg = event.text.split(" ");
+            if (!isNaN(Number(msg[1])))
+            {
+                let command = msg[0];
+                let arg = Number(msg[1]);
+
+                switch (command)
+                {
+                    case "-gold_scale":
+                        GOLD_MODIFIER = arg;
+                        break;
+                    case "-xp_scale":
+                        XP_MODIFIER = arg;
+                        break;
+                    case "-respawn_scale":
+                        RESPAWN_MODIFIER = arg;
+                        this.Game.SetRespawnTimeScale(arg);
+                        break;
+                    case "-creep_scale":
+                        CREEP_SCALE = arg;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+    
 }
